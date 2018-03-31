59da4c526b6355f12b32132263b305d4acddc039
ThePlifff <33992498+ThePlifff@users.noreply.github.com>
2018-03-31 16:20:01 -0400

    Add files via upload

--- contact-script.js
+++ contact-script.js
@@ -6,3 +6,20 @@ function myMap() {
     }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('email-popup').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('email-popup').style.display = "none";
+}
